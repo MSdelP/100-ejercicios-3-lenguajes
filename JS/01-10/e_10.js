6db85a067b2f5a1f4702104902ef28a3eab4290c
MSdelP <145930056+MSdelP@users.noreply.github.com>
2024-01-26 11:44:35 +0100

    Ejercicios

--- JS/01-10/e_10.js
+++ JS/01-10/e_10.js
@@ -3,4 +3,4 @@ let numeros = Array.from({length: 10}, (_, index) => index + 1);
 
 // Esta instrucción utiliza Array.from junto con una función de mapeo para generar un array que contiene los números del 1 al 10. 
 // La función de mapeo recibe dos argumentos, el primer argumento (_) se ignora ya que no se utiliza,
-// el segundo argumento (index) representa el índice actual en el array, y se suma 1 para obtener los números del 1 al 10.
+// el segundo argumento (index) representa el índice actual en la lista, y se suma 1 para obtener los números del 1 al 10.
