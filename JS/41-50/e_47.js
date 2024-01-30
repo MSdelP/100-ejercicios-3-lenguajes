c5fa9ec5b5d46cefbecb9a3ce2db6b7fa2bf0640
MSdelP <145930056+MSdelP@users.noreply.github.com>
2024-01-30 22:30:54 +0100

    Ejercicios

---
+++ JS/41-50/e_47.js
@@ -0,0 +1,18 @@
+function minimo(L) {
+    if (L.length === 0) {
+        // Manejar el caso de un array vacío
+        return null;
+    }
+
+    // Utilizar Math.min para obtener el valor mínimo del array
+    return Math.min(...L);
+}
+
+// Ejemplo de uso
+const arrayNumeros = [10, 5, 8, 15, 3];
+const resultado = minimo(arrayNumeros);
+console.log(resultado);
+
+
+// En este ejemplo, la función minimo utiliza el operador de propagación (...) para pasar los elementos del array como argumentos individuales a la función Math.min.
+// Si el array está vacío, la función devuelve null. Puedes probar la función con diferentes arrays de enteros para encontrar el valor mínimo.
