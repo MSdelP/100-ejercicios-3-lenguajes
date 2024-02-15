9ee6c4c2c0c70ba8b7f58508edb92fbf5c534bc3
MSdelP <marsandelpoz@gmail.com>
2024-02-15 20:31:32 +0100

    Ejercicios

---
+++ Python/51-60/e_53.py
@@ -0,0 +1,3 @@
+# Escribe una función en Python numValoresDicc(d) que tome un diccionario como parámetro,
+# y devuelva el número de valores contenidos en las listas asociadas a las claves
+# Nota: A efectos de este ejercicio consideramos que todos los valores asociados a las claves están en forma de lista.
