545b39a53bea5b4eca4c4995fcb7944ec8878803
MSdelP <marsandelpoz@gmail.com>
2024-02-18 20:05:42 +0100

    Ejercicios

--- Java/src/e51/e_56.java
+++ Java/src/e51/e_56.java
@@ -1,3 +1,22 @@
-public class e_56 {
+import java.util.ArrayList;
 
+public class e_56 {
+    public static ArrayList<Integer> divMult(int n, int a, int numUmbral) {
+        ArrayList<Integer> numerosEncontrados = new ArrayList<>();
+        for (int num = 0; num <= numUmbral; num++) {
+            if (num % n == 0 && num % a != 0) {
+                numerosEncontrados.add(num);
+            }
+        }
+        return numerosEncontrados;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int a = 4;
+        int numUmbral = 20;
+        ArrayList<Integer> resultado = divMult(n, a, numUmbral);
+        System.out.println(
+                "Números divisibles por " + n + " y no múltiplos de " + a + " hasta " + numUmbral + ": " + resultado);
+    }
 }
