c0d2e2bb02b889635745345ce14bd2aa7fe3a768
MSdelP <marsandelpoz@gmail.com>
2024-02-22 19:04:34 +0100

    Ejercicios

---
+++ Python/61-70/e_63.py
@@ -0,0 +1,4 @@
+# Escribe una función en Python  llamada cambiaPalabras(frase,cambiaPalabra,palabra), 
+# que tome como parámetros una frase y filtre las palabras deseadas en cambiaPalabra (puede ser una o varias) y las cambie por el elemento declarado en palabra. 
+# La función debe devolver la misma frase con las palabras filtradas.
+
