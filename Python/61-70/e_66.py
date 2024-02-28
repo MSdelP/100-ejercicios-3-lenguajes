2da80318eb052eec5fdc9d195a085154ab253d71
MSdelP <marsandelpoz@gmail.com>
2024-02-28 23:41:23 +0100

    Ejercicios

---
+++ Python/61-70/e_66.py
@@ -0,0 +1,3 @@
+# Escribe una función en Python unionLista(L1,L2,L3) que toma como parámetros las listas de enteros L1, L2, y L3, 
+# y devuelva una lista en orden asccendente de estas listas sin repetición de números, 
+# los números repetidos duplicados serán omitido.s
