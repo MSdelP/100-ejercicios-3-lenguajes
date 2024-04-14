e3bc1b5d4cc777db4fb1dfeb46cc393c31e4fe15
MSdelP <marsandelpoz@gmail.com>
2024-04-14 23:42:52 +0200

    Ejercicios

---
+++ JS/61-70/e_67.js
@@ -0,0 +1,5 @@
+// Escribe un función en JS calculoMCD(a,b) que tome como parámetros dos enteros que introduce el usuario, a y b. 
+// Después calcula el máximo común divisor utilizando el algoritmo de Euclides.
+// Nota: La división euclidiana de a por b se escribe de la siguiente manera:
+// a = b*q + r   
+// q representa el cociente y r es el resto de la división.
